ceccb23b5d37d566aaa0bba23b4dfbcbf5ba8e0c
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-11-27 09:05:01 -0600

    Getting rid of FindReplace old stuff, making the csproj more generic by calling it AvalonEdit.Pieces.  Then also putting in a empty LineNumberMarginsWithCommands

---
+++ AvalonEdit.Pieces/LineNumberMarginWithCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.AvalonEdit;
+
+namespace AvalonEdit.Pieces
+{
+    // idea from: http://community.icsharpcode.net/forums/t/11706.aspx
+    class LineNumberMarginWithCommands : ICSharpCode.AvalonEdit.Editing.LineNumberMargin
+    {
+
+        public static void Install( TextEditor editor)
+        {
+
+        }
+
+
+
+
+    }
+}
